fcb89eadc03b203858941428d71edb266928c1d9
Alex Kuzmin <alex.kuzmin@proton.me>
2024-09-10 14:15:16 +0800

    Make proof inputs interactive

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,6 +22,17 @@ class _MyAppState extends State<MyApp> {
   bool isProving = false;
   PlatformException? _error;
 
+  // Controllers to handle user input
+  final TextEditingController _controllerA = TextEditingController();
+  final TextEditingController _controllerB = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controllerA.text = "5";
+    _controllerB.text = "3";
+  }
+
   @override
   Widget build(BuildContext context) {
     // The inputs is a base64 string
@@ -38,20 +49,100 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Flutter App With MoPro'),
         ),
-        body: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            if (isProving) const CircularProgressIndicator(),
-            if (_error != null)
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              if (isProving) const CircularProgressIndicator(),
+              if (_error != null)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(_error.toString()),
+                ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text(_error.toString()),
+                child: TextFormField(
+                  controller: _controllerA,
+                  decoration: const InputDecoration(
+                    labelText: "Public input `a`",
+                    hintText: "For example, 5",
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
               ),
-            if (_proofResult != null)
-              SingleChildScrollView(
-                child: Column(
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _controllerB,
+                  decoration: const InputDecoration(
+                    labelText: "Public input `b`",
+                    hintText: "For example, 3",
+                  ),
+                  keyboardType: TextInputType.number,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlinedButton(
+                        onPressed: () async {
+                          if (_controllerA.text.isEmpty ||
+                              _controllerB.text.isEmpty ||
+                              isProving) {
+                            return;
+                          }
+                          setState(() {
+                            isProving = true;
+                          });
+
+                          FocusManager.instance.primaryFocus?.unfocus();
+                          Map<String, dynamic>? proofResult;
+                          PlatformException? error;
+                          // Platform messages may fail, so we use a try/catch PlatformException.
+                          // We also handle the message potentially returning null.
+                          try {
+                            var inputs = <String, List<String>>{};
+                            inputs["a"] = [_controllerA.text];
+                            inputs["b"] = [_controllerB.text];
+                            proofResult =
+                                await _moproFlutterPlugin.generateProof(
+                                    "assets/multiplier2_final.zkey", inputs);
+                            setState(() {
+                              isProving = false;
+                            });
+                            print(
+                              "Proof result: $proofResult",
+                            );
+                          } on PlatformException catch (e) {
+                            print("Error: $e");
+                            error = e;
+                            proofResult = null;
+                          }
+
+                          // If the widget was removed from the tree while the asynchronous platform
+                          // message was in flight, we want to discard the reply rather than calling
+                          // setState to update our non-existent appearance.
+                          if (!mounted) return;
+
+                          setState(() {
+                            _proofResult = proofResult == null
+                                ? null
+                                : GenerateProofResult(proofResult["proof"],
+                                    proofResult["inputs"]);
+                            _error = error;
+                          });
+                        },
+                        child: const Text(
+                          "Generate Proof",
+                        )),
+                  ),
+                ],
+              ),
+              if (_proofResult != null)
+                Column(
                   children: [
                     Padding(
                       padding: const EdgeInsets.all(8.0),
@@ -63,57 +154,8 @@ class _MyAppState extends State<MyApp> {
                     ),
                   ],
                 ),
-              ),
-            Flexible(
-              flex: 1,
-              child: Container(),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlinedButton(
-                      onPressed: () async {
-                        Map<String, dynamic>? proofResult;
-                        PlatformException? error;
-                        // Platform messages may fail, so we use a try/catch PlatformException.
-                        // We also handle the message potentially returning null.
-                        try {
-                          var inputs = <String, List<String>>{};
-                          inputs["a"] = ["3"];
-                          inputs["b"] = ["5"];
-                          proofResult = await _moproFlutterPlugin.generateProof(
-                              "assets/multiplier2_final.zkey", inputs);
-                          print(
-                            "Proof result: $proofResult",
-                          );
-                        } on PlatformException catch (e) {
-                          print("Error: $e");
-                          error = e;
-                          proofResult = null;
-                        }
-
-                        // If the widget was removed from the tree while the asynchronous platform
-                        // message was in flight, we want to discard the reply rather than calling
-                        // setState to update our non-existent appearance.
-                        if (!mounted) return;
-
-                        setState(() {
-                          _proofResult = proofResult == null
-                              ? null
-                              : GenerateProofResult(
-                                  proofResult["proof"], proofResult["inputs"]);
-                          _error = error;
-                        });
-                      },
-                      child: const Text(
-                        "Generate Proof",
-                      )),
-                ),
-              ],
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
